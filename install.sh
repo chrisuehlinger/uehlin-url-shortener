e9d97d88ed26a05ebc6cc5df96cab4a17c1eb65f
Chris Uehlinger <chris.uehlinger@gmail.com>
2018-07-24 18:14:21 -0400

    Add systemd service file

--- install.sh
+++ install.sh
@@ -7,4 +7,8 @@ sudo apt-get install -y docker-ce docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+sudo cp docker-compose-app.service /etc/systemd/system/docker-compose-app.service
+systemctl enable docker-compose-app
+
 sudo reboot
